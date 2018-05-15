cf2faddaec9a962dcaaefaf2b6873cc7583f2caf
Abhijit Khanna <hindu_steve@yahoo.com>
2018-05-15 11:02:40 -0400

    Added ability to prompt user for string to SolutionStreet1.js

--- SolutionStreet1.js
+++ SolutionStreet1.js
@@ -1,23 +1,25 @@
 var base_string = "" // variable to store input
-var target_string = "" // variable for output string, where duplicate characters are removed
 var stringArray = [] // empty stringArray
 
 // defining a function to count the frequency of each unique character, accepts an argument of an array from the input string
 function characterCount(charArray) {
-	
-	var outputArray = []; // array to contain unique characters
-	var currentChar = ""; 
 
-	for (var i=0; i < charArray.length; i++) {
-		currentChar = charArray[i];
-		if (outputArray.indexOf(currentChar) === -1) {
-			outputArray.push(charArray[i]);
-		}
-	}
-	return outputArray.join("");
+  var outputArray = []; // array to contain unique characters
+  var currentChar = ""; 
+
+  for (var i=0; i < charArray.length; i++) {
+    //currentChar = charArray[i];
+    if (charArray[i] !== " ") { // check for spaces
+      if (outputArray.indexOf(charArray[i]) === -1) { // check if current charactaer already exists in output array
+        outputArray.push(charArray[i]);
+      }
+    } else { // add spaces to output array no matter what
+        outputArray.push(charArray[i]);    
+    }
+  }
+  return outputArray.join("");
 }
 
-
-base_string = "tree traversal";
+base_string = prompt("Please enter a string for which the duplicate characters should be removed");
 stringArray = base_string.split("");
-characterCount(stringArray);
+console.log(characterCount(stringArray))
